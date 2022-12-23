28c52d6b3499b87bf21037f4ee3e883e10851dc5
Michael Cavallaro <michaelcavallaro03@gmail.com>
2022-12-23 15:32:10 -0500

    Setup typescript

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,16 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "turbo",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "@typescript-eslint/no-non-null-assertion": "warn",
   },
+  ignorePatterns: ["**/dist"],
 };
